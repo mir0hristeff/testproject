2a00d7ce27aec20eda176946c7cd2af9411f59ef
Miroslav Hristev <mhristev@sevone.com>
2018-10-25 15:32:14 +0300

    gatling with scala

---
+++ src/test/scala/TestSimulation.scala
@@ -0,0 +1,18 @@
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class TestSimulation extends Simulation {
+
+  val url = "http://dummy.restapiexample.com/"
+  val httpProtocol = http
+    .baseUrl(url)
+
+  val getSpecificEmployee = "api/v1/employee/885"
+
+  val getByIdScenario = scenario("getById")
+    .exec(http("request_1")
+      .get(getSpecificEmployee))
+
+  setUp(getByIdScenario.inject(atOnceUsers(5))).protocols(httpProtocol)
+
+}
